49843c6b77eb486753d394b83919bc7edd7241c2
John Patrick A. Hawac <johnpatrick.hawac@neu.edu.ph>
2026-04-02 22:45:38 +0800

    fixed app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import { AuthProvider } from './context/AuthContext';
-import { Routes, Route } from 'react-router-dom'
-import Login from './pages/auth/Login'
-import Register from './pages/auth/Register'
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <AuthProvider>
-      <YourRouterComponents />
-      </AuthProvider>
+    <AuthProvider>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
+    </AuthProvider>
   );
 }
 
-export default App
+export default App;
