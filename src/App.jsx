b76c1b663f3aea3a490f1d8a3a7f8fa43ab81fe9
Jorus Junio <jorusjunio28@gmail.com>
2026-04-06 20:21:41 +0800

    Finalize routing for PR4

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
 import AuthCallback from './pages/auth/AuthCallback' 
-import AppShell from './components/layout/AppShell' // I-uncomment mo na 'to
+import AppShell from './components/layout/AppShell'
 
 function App() {
   return (
