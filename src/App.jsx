c8f107a289b3ff13f26bc69619eeeebaa23f7485
viio00 <194786916+viio00@users.noreply.github.com>
2026-04-17 12:13:19 +0800

    Added customerService file with methods, supabase moved in src. Will add testing files later.

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,28 @@
-import { supabase } from "../supabase/supabaseClient";
+import { supabase } from "./supabase/supabaseClient";
 import { AuthProvider } from "./context/AuthContext";
-import { Routes, Route } from "react-router-dom";
-import Login from "./pages/auth/Login";
-import Register from "./pages/auth/Register";
-import AppShell from "./components/layout/AppShell";
-import AuthCallback from "./pages/auth/AuthCallback";
+import { Routes, Route } from 'react-router-dom'
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
+import AuthCallback from './pages/auth/AuthCallback'
+import AppShell from './components/layout/AppShell'
 
 function App() {
   return (
     <AuthProvider>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/auth/callback" element={<AuthCallback />} />
-        <Route
-          path="/"
-          element={
-            <AppShell>
-              <p style={{ color: "rgba(180,210,255,0.4)", fontSize: "13px" }}>
-                Dashboard content here
-              </p>
-            </AppShell>
-          }
-        />
-      </Routes>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/auth/callback" element={<AuthCallback />} />
+      <Route path="/*" element={
+        <AppShell>
+          <p style={{ color: 'rgba(180,210,255,0.4)', fontSize: '13px' }}>
+            Dashboard content here
+          </p>
+        </AppShell>
+      } />
+    </Routes>
     </AuthProvider>
   );
 }
 
-/* Add this temporary block to your code to test the DB Seeding
-const testConnection = async () => {
-  const { data, error } = await supabase.from('customer').select('').limit(5);
-
-  if (error) {
-    console.error("❌ Connection Failed:", error.message);
-  } else {
-    console.log("✅ Success! DB Data found:", data);
-  }
-};*/
-
-//testConnection();
-
-export default App;
+export default App
